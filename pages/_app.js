0a9961d1bc3b1dd02d3bb9bd9ba9cab4f83e7f5f
Ogundele Caleb <ogundelecaleb13@gmail.com>
2022-11-02 01:35:02 +0100

    new commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,35 +1,27 @@
-import React from 'react';
-import { Toaster } from 'react-hot-toast';
+import React from "react";
+import { Toaster } from "react-hot-toast";
 
-import { Layout } from '../components';
-import '../styles/globals.css';
-import { StateContext } from '../context/StateContext';
+import { Layout } from "../components";
+import "../styles/globals.css";
+import { StateContext } from "../context/StateContext";
 import "../styles/globals.css";
 import "../src/config/firebase.config";
 import { AuthProvider } from "../src/hook/auth";
 import AuthStateChanged from "../src/layout/AuthStateChanged";
 
-
 function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
       <StateContext>
-      <Layout>
-        <Toaster />
-        <AuthStateChanged>
-          <Component {...pageProps} />
-        </AuthStateChanged>
-        
-      </Layout>
-    </StateContext>
+        <Layout>
+          <AuthStateChanged>
+            <Toaster />
+            <Component {...pageProps} />
+          </AuthStateChanged>
+        </Layout>
+      </StateContext>
     </AuthProvider>
-    
-  )
+  );
 }
 
-export default MyApp
-
-
-
-
-
+export default MyApp;
